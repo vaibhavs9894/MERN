9808379600b841fa13419fe610756c5c466aaf1a
vaibhavs9894 <vaibhavshrimali@gmail.com>
2024-06-04 15:15:10 +0530

    day 15

---
+++ DAY 15 -react how to add images and layout/frontend/src/app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', fontSize: '56px', color: 'red' }} >Home Page</h1>
+      
+      <input type="text" className='border-2 border-black' />
+    
+    </div>
+  )
+}
+
+export default Home;
